e2be9bdb54c3ec81addd8bb562247f968cc3ce17
josemejias081 <josemejias081@gmail.com>
2022-08-01 03:01:48 +0200

    more change now sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,12 +99,12 @@ Rails.application.configure do
 
 
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV["EMAIL_USERNAME"],
-    :password             => ENV["EMAIL_PASSWORD"],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  #config.action_mailer.smtp_settings = {
+   # :address              => "smtp.gmail.com",
+    #:port                 => 587,
+    #:user_name            => ENV["EMAIL_USERNAME"],
+    #:password             => ENV["EMAIL_PASSWORD"],
+    #:authentication       => "plain",
+    #:enable_starttls_auto => true
+  #}
 end
