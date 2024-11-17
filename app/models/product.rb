92b7e952cc8ec57d69a9b54a56f01d9921265e07
josemejias081 <josemejias081@gmail.com>
2024-11-17 11:11:21 +0100

    added JS function in the product form for calculate sales price of the product

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,6 +17,10 @@ class Product < ApplicationRecord
   validates :photo, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
   validate :photo_validation
 
+  
+
+
+
   def sale_price
     cost + (cost * (markup_percentage / 100))
   end
